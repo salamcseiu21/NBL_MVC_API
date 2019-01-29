afb084f6961d4ef69a98d6fbc65f14db77f66be1
Md.Abdus Salam <salamcseiu21@gmail.com>
2019-01-29 17:24:50 +0600

    Code Modification

--- NBL.BLL/ClientManager.cs
+++ NBL.BLL/ClientManager.cs
@@ -90,10 +90,10 @@ namespace NBL.BLL
         public IEnumerable<Client> GetClientByBranchId(int branchId)
         {
             var clients = _iClientGateway.GetClientByBranchId(branchId);
-            //foreach (Client client in clients)
-            //{
-            //    client.Orders = _iOrderManager.GetOrdersByClientId(client.ClientId).ToList();
-            //}
+            foreach (Client client in clients)
+            {
+                client.Orders = _iOrderManager.GetOrdersByClientId(client.ClientId).ToList();
+            }
             return clients;
         }
 
---
+++ NBL/obj/Release/Package/PackageTmp/Areas/Sales/Scripts/ReceiveProduct/loadAllReceivableProducts.js
@@ -0,0 +1,34 @@
+﻿function loadAllReceivableProducts() {
+    var deliveryId = $("#DeliveryId").val();
+    $.ajax({
+        type: 'GET',
+        url: RootUrl + "Sales/Product/LoadReceiveableProduct",
+        contentType: 'application/json; charset=utf-8',
+        dataType: 'json',
+        data: { deliveryId: deliveryId },
+        success: function (data) {
+            $('#tbl_receivable_list').dataTable({
+                destroy: true,
+                data: data,
+                columns: [
+                    { 'data': 'ProductName' },
+                    { 'data': 'Quantity' },
+                    {
+                        data: null,
+                        className: "text-center",
+                        render: function (data, type, row) {
+                            return data.ProductBarCodes;
+                        }
+                    },
+                    {
+                        data: null,
+                        className: "text-center",
+                        render: function (data, type, row) {
+                            return data.RecievedProductBarCodes;
+                        }
+                    }
+                ]
+            });
+        }
+    });
+}
